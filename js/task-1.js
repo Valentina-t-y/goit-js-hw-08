967b5cfa265c7af29e9efc2ce2eef99d9f615a6b
Valentina-t-y <146077293+Valentina-t-y@users.noreply.github.com>
2024-04-03 12:43:43 +0300

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+function slugify(title) {
+    let arr = title.split(" ");
+    let arr2 = arr.join("-").toLowerCase();
+    return arr2;
+}
+
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
